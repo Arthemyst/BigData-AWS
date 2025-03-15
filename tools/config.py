e4bfba32c95a379d0006511412db75291b1cfb1f
Arthemyst <pawcio262@gmail.com>
2025-03-15 12:27:04 +0100

    changed _aws_password for _aws_access_key and _aws_user for _aws_key_id

--- tools/config.py
+++ tools/config.py
@@ -9,13 +9,13 @@ class CustomEnvironment:
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
-    _aws_password = env.str("AWS_PASSWORD")
-    _aws_user = env.str("AWS_USER")
+    _aws_access_key = env.str("AWS_ACCESS_KEY")
+    _aws_key_id = env.str("AWS_KEY_ID")
 
     @classmethod
     def get_aws_password(cls) -> str:
-        return cls._aws_password
+        return cls._aws_access_key
 
     @classmethod
     def get_aws_user(cls) -> str:
-        return cls._aws_user
+        return cls._aws_key_id
