003d5de32f6e86ed2d94f8cc8698877d712ef4d1
Arthemyst <pawcio262@gmail.com>
2025-03-29 13:19:04 +0100

    refactored analyze_data.py to use it in aws lambda

--- analyze_data.py
+++ analyze_data.py
@@ -1,23 +1,27 @@
-import os
-import boto3
 import json
+import os
 
+import boto3
+import pandas as pd
+
+from tools.config import CustomEnvironment
+
+bucket_name = CustomEnvironment.get_aws_s3_bucket()
 s3_client = boto3.client('s3')
 
 
 def process_file(file_name, bucket_name):
     local_path = '/tmp/' + os.path.basename(file_name)
     print(f"Loading file: {file_name} from S3")
-
     s3_client.download_file(bucket_name, file_name, local_path)
 
+    data = pd.read_json(local_path)
     print(f"Analysing file: {file_name}")
-
+    count_of_pages = data.groupby('page').size().reset_index(name='count')
+    print(count_of_pages)
     output_filename = file_name.split(".")[0]
     output_file = '/tmp/processed_' + os.path.basename(output_filename) + '.csv'
-    with open(output_file, 'w') as f:
-        f.write('')
-
+    count_of_pages.to_csv(output_file, index=False)
     s3_client.upload_file(output_file, bucket_name, f'processed/{os.path.basename(output_file)}')
     print(f"File processed and saved to: processed/{os.path.basename(output_file)}")
 
